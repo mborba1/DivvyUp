13d46fdb6036edbc55f51cdb8e6c6992d57ace9f
AnjiNai <anjali.nainani@gmail.com>
2021-08-20 11:53:08 -0400

    pushing-up-for-my-team

---
+++ config/firebase.js
@@ -0,0 +1,24 @@
+// AN Note: This is importing everything from firebase,
+import * as firebase from 'firebase';
+// This imports environment (so all your secret keys).
+import Environment from "./environment"
+
+// This says that if there is not an existing firebase db/storage,
+// Then initialize a new app and name it divvyup.
+console.log(firebase.apps)
+// I needed to add this line checking if there was an existing firebase, if not, a new one is initialized.
+if (firebase.apps.length === 0){
+    console.log(firebase.apps)
+firebase.initializeApp({
+    apiKey: Environment['FIREBASE_API_KEY'],
+    authDomain: Environment['FIREBASE_AUTH_DOMAIN'],
+    databaseURL: Environment['FIREBASE_DATABASE_URL'],
+    projectId: Environment['FIREBASE_PROJECT_ID'],
+    storageBucket: Environment['FIREBASE_STORAGE_BUCKET'],
+    messagingSenderId: Environment['FIREBASE_MESSAGING_SENDER_ID'],
+    //appId: Environment['FIREBASE_APP_ID']
+    },"divvyup");
+}
+
+// This is running and exporting this entire file.
+export default firebase;
