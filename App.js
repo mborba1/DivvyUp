1d354bad301c448e3ec8b01e1814185de64bbbb3
Josephine <80869118+joseewang@users.noreply.github.com>
2021-08-23 11:44:27 -0400

    feat: signup creates user in users collection via google auth uid

--- App.js
+++ App.js
@@ -117,7 +117,7 @@ export default class App extends React.Component {
 					</View>
 				</ScrollView>
         <Modal>
-          <Login />
+          <Signup />
         </Modal>
 			</View>
 		);
