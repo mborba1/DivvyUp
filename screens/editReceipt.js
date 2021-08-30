78411839f3e3aca20935ed5b199e57413ac7ee60
AnjiNai <anjali.nainani@gmail.com>
2021-08-30 16:02:27 -0400

    added styling to edit page, deleted extraneous files

--- screens/editReceipt.js
+++ screens/editReceipt.js
@@ -1,79 +1,82 @@
-import React, { useContext, useState } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  ImageBackground,
-  FlatList,
-  ViewStyle,
-  TouchableOpacity
-} from 'react-native';
-import Button from '../components/Button'
-import { TextInputMask } from 'react-native-masked-text'
-import { auth } from '../config/firebase';
-import { AuthenticatedUserContext } from '../navigation/AuthenticatedUserProvider';
-import firebase from '../config/firebase'
+import React, {useContext, useState} from 'react';
+import {StyleSheet, Text, View, ImageBackground, FlatList} from 'react-native';
+import {TextInputMask} from 'react-native-masked-text';
+import {auth} from '../config/firebase';
+import {AuthenticatedUserContext} from '../navigation/AuthenticatedUserProvider';
+import firebase from '../config/firebase';
 const firestore = firebase.firestore();
+import Header from './header';
+import {Button} from 'react-native-paper';
 
+export default EditReceipt = ({route}) => {
+  const {
+    img,
+    text,
+    button,
+    container,
+    textHeader,
+    list,
+    listItem,
+    separator,
+    textInput,
+  } = styles;
+  const {user} = useContext(AuthenticatedUserContext);
+  const [receipt, setReceipt] = useState(route.params.receipt);
 
-export default EditReceipt = ({ route }) => {
-  const {img, text, button, container} = styles;
-  const { user } = useContext(AuthenticatedUserContext) 
-  const [receipt, setReceipt] = useState(route.params.receipt)
-
-  function submitReceipt(){
-   return (
-       firestore.collection('receipts')
-       .add({receipt: {...receipt, charger: `${user.uid}`}})
-   )        
+  function submitReceipt() {
+    return firestore
+      .collection('receipts')
+      .add({receipt: {...receipt, charger: `${user.uid}`}});
   }
 
-  function updateItemPrice(item, newPrice){
-    const items = receipt.items
-    item.price = newPrice
-    setReceipt({...receipt, items: items, charger: `${user.uid}`})
+  function updateItemPrice(item, newPrice) {
+    const items = receipt.items;
+    item.price = newPrice;
+    setReceipt({...receipt, items: items, charger: `${user.uid}`});
   }
-  
-  function listItems(){
-      return (
-          <View  style={styles.list} >
-            <FlatList
-              ListHeaderComponent={()=><Text style={styles.textHeader} >ITEM</Text>}
-              ItemSeparatorComponent={() =><View style={styles.separator} />}
-              data={receipt.items}
-              renderItem={({item}) => { 
-                return (
-                  <View
-                  style={styles.listItem} 
-                  key={item.key}>
-                      <Text style={styles.text}>{item.description}</Text>
-                  </View>
-                )}}           
-            />
-            <FlatList
-             ListHeaderComponent={()=><Text style={styles.textHeader}>PRICE</Text>}
-             ItemSeparatorComponent={() =><View style={styles.separator} />}
-              data={receipt.items}
-              renderItem={({item}) => { 
-                return (
-                  <View
-                  style={styles.listItem} 
-                  key={item.key}>
-                      <TextInputMask
-                      style={styles.textInput} 
-                      type={'money'}
-                      options={{
-                        separator: '.',
-                        delimiter: ',',
-                        unit: null
-                      }}
-                      value={item.price}
-                      onChangeText={newPrice => updateItemPrice(item, newPrice)}/>
-                  </View>
-                )}}           
-            />
-          </View>
-    )}
+
+  function listItems() {
+    return (
+      <View>
+        <View style={list}>
+          <FlatList
+            ListHeaderComponent={() => <Text style={textHeader}>ITEM</Text>}
+            ItemSeparatorComponent={() => <View style={separator} />}
+            data={receipt.items}
+            renderItem={({item, index}) => {
+              return (
+                <View style={listItem} key={`${index}item`}>
+                  <Text style={text}>{item.description}</Text>
+                </View>
+              );
+            }}
+          />
+          <FlatList
+            ListHeaderComponent={() => <Text style={textHeader}>COST</Text>}
+            ItemSeparatorComponent={() => <View style={separator} />}
+            data={receipt.items}
+            renderItem={({item, index}) => {
+              return (
+                <View style={listItem} key={`${index}cost`}>
+                  <TextInputMask
+                    style={textInput}
+                    type={'money'}
+                    options={{
+                      separator: '.',
+                      delimiter: ',',
+                      unit: null,
+                    }}
+                    value={item.price}
+                    onChangeText={newPrice => updateItemPrice(item, newPrice)}
+                  />
+                </View>
+              );
+            }}
+          />
+        </View>
+      </View>
+    );
+  }
 
   if (!receipt) {
     return (
@@ -83,41 +86,42 @@ export default EditReceipt = ({ route }) => {
     );
   } else {
     return (
-        <View style={styles.container}>
-          <ImageBackground
-            style={img}
-            source={require('../assets/divvyup-background.jpg')}
-            resizeMode="cover">
-              <View style={styles.container}>
-                  {listItems()}
-                  <Button style={styles.button} title="Submit" onPress={submitReceipt}/>
-              </View> 
-          </ImageBackground>
-        </View>
+      <View style={container}>
+        <ImageBackground
+          style={img}
+          source={require('../assets/divvyup-background.jpg')}
+          resizeMode="cover">
+          <Header />
+          <View style={styles.constainerForListAndSubmit}>
+            {listItems()}
+            <Button style={button} onPress={submitReceipt} mode="contained">
+              Submit
+            </Button>
+          </View>
+        </ImageBackground>
+      </View>
     );
   }
 };
 
-
 const styles = StyleSheet.create({
-
   img: {
     flex: 1,
-    justifyContent: 'space-around'
+    justifyContent: 'space-around',
   },
   container: {
-    flex: 1, 
-    flexDirection: 'column'
-  },  
+    flex: 1,
+    flexDirection: 'column',
+  },
   list: {
     marginTop: 60,
     flexDirection: 'row',
-    padding: 25
+    padding: 25,
   },
   listItem: {
     padding: 10,
     height: 50,
-    justifyContent: 'flex-end'
+    justifyContent: 'flex-end',
   },
   text: {
     fontSize: 20,
@@ -137,26 +141,21 @@ const styles = StyleSheet.create({
   textInput: {
     paddingHorizontal: 2,
     textAlign: 'center',
-    marginHorizontal: 10, 
-    width: 60, 
+    marginHorizontal: 10,
+    width: 60,
     backgroundColor: '#fff',
     borderRadius: 3,
   },
   button: {
-    width: '10',
-    height: '40',
     color: 'white',
-    fontFamily: 'Lato_400Regular',
-    fontSize: 2,
-    textAlign: 'center',
-    alignItems: 'center',
-    padding: 5,
-    margin: 5,
-    borderRadius: 10,
-  }, 
+    backgroundColor: 'black',
+  },
   separator: {
-    height: 1, 
-    width: "100%",
-    backgroundColor: "#CEDCCE",
-  }
+    height: 1,
+    width: '100%',
+    backgroundColor: '#CEDCCE',
+  },
+  constainerForListAndSubmit: {
+    height: '85%',
+  },
 });
